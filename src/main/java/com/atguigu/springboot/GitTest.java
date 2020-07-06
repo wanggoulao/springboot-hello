e55eb63843c284ce5fbcd01d89ad1fa77ac6d501
王艺鹏 <3070185329@qq.com>
2020-07-06 21:21:25 +0800

    this is a small github practice

--- src/main/java/com/atguigu/springboot/GitTest.java
+++ src/main/java/com/atguigu/springboot/GitTest.java
@@ -6,6 +6,6 @@ package com.atguigu.springboot;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("1111111");
+        System.out.println("1111111AAAAAAAAAAAA");
     }
 }

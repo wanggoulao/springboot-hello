68456e7c86e41a2aad8cbbfa5c24f10227c590bc
王艺鹏 <3070185329@qq.com>
2020-07-06 21:17:11 +0800

    this is a small github practice

---
+++ src/main/java/com/atguigu/springboot/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.springboot;
+
+/**
+ * @author Administrator
+ * @create 2020-07-06 21:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("1111111");
+    }
+}
